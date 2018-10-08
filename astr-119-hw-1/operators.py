05e3bb8776811922f3548871b66d9ba57599772b
Merlin1908 <43654433+Merlin1908@users.noreply.github.com>
2018-10-08 15:25:51 -0700

    those are my homework#1

---
+++ astr-119-hw-1/operators.py
@@ -0,0 +1,35 @@
+x = 9
+y = 3	#integers
+
+#arithmetic operators
+print(x+y)	#addtion
+print(x-y)	#subtraction
+print(x*y)	#multiplication
+print(x/y)	#division
+print(x%y)	#modulus
+print(x**y)	#exponentiation x to the power of y x^y
+x = 9.1918123
+print(x//y)	#floor division
+
+#assignment operators
+x = 9		#sets x to equal 9
+x += 3		#x = x + 3
+print(x)
+x = 9
+x -=3		#x = x - 3
+print(x)
+x *= 3		#x = x * 3
+print(x)
+x /= 3		#x = x / 3
+print(x)
+x **= 3		#x = x**3
+print(x)
+
+#comparison operators
+x = 9
+y = 3
+print(x==y)		#will print True if x equals y, False otherwise
+print(x!=y)		#True is x does not eq y, False otherwise
+print(x>y)
+print(x<y)
+print(x>=y)		#true if x is gt or eq to y, false otherwise
